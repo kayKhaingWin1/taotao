11f0615a931e0379c8929cea20b4f15ce3f5829a
kayKhaingWin1 <helilin15@gmail.com>
2025-08-14 14:16:15 +0630

    update size& user structure

--- model/Size.php
+++ model/Size.php
@@ -19,11 +19,13 @@ class Size
     }
    
 public function getSizesByProductId($productId) {
-    $sql = "SELECT DISTINCT s.* FROM size s
-            JOIN product_size_color psc ON s.id = psc.size_id
-            WHERE psc.product_id = :product_id
+    $sql = "SELECT * FROM (
+                SELECT DISTINCT s.* FROM size s
+                JOIN product_size_color psc ON s.id = psc.size_id
+                WHERE psc.product_id = :product_id
+            ) AS distinct_sizes
             ORDER BY 
-                CASE s.size
+                CASE size
                     WHEN 'XS' THEN 1
                     WHEN 'S' THEN 2
                     WHEN 'M' THEN 3
@@ -32,7 +34,7 @@ public function getSizesByProductId($productId) {
                     WHEN 'XXL' THEN 6
                     ELSE 7
                 END";
-    $this->statement = $this->conn->prepare($sql);
+     $this->statement = $this->conn->prepare($sql);
     $this->statement->bindParam(':product_id', $productId);
     $this->statement->execute();
     return $this->statement->fetchAll(PDO::FETCH_ASSOC);
