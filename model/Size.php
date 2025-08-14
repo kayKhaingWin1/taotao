556e324f4dbc18243e6c320912b3f6c3319cc010
kayKhaingWin1 <helilin15@gmail.com>
2025-08-14 13:58:49 +0630

    update size

--- model/Size.php
+++ model/Size.php
@@ -18,15 +18,23 @@ class Size
         return $this->statement->fetchAll(PDO::FETCH_ASSOC);
     }
    
-   public function getSizesByProductId($productId) {
+public function getSizesByProductId($productId) {
     $sql = "SELECT DISTINCT s.* FROM size s
             JOIN product_size_color psc ON s.id = psc.size_id
             WHERE psc.product_id = :product_id
-            ORDER BY FIELD(s.size, 'XS', 'S', 'M', 'L', 'XL', 'XXL')";
+            ORDER BY 
+                CASE s.size
+                    WHEN 'XS' THEN 1
+                    WHEN 'S' THEN 2
+                    WHEN 'M' THEN 3
+                    WHEN 'L' THEN 4
+                    WHEN 'XL' THEN 5
+                    WHEN 'XXL' THEN 6
+                    ELSE 7
+                END";
     $this->statement = $this->conn->prepare($sql);
     $this->statement->bindParam(':product_id', $productId);
     $this->statement->execute();
     return $this->statement->fetchAll(PDO::FETCH_ASSOC);
 }
-
 }
